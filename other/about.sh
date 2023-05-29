229e42d07b11436364328c62a303ae68787e786e
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-29 08:54:32 +0700

    Update about.sh

--- other/about.sh
+++ other/about.sh
@@ -10,17 +10,67 @@ WB='\e[37;1m'
 clear
 echo ""
 echo ""
-echo -e "${RB}—————————————————————————————————————————————————————————${NC}"
-echo -e "                  ${GB}Xray Mini Premium${NC}"
-echo -e "${RB}—————————————————————————————————————————————————————————${NC}"
-echo -e "  ${GB}»»» Protocol Service «««  |  »»» Network Protocol «««${NC}  "
-echo -e "${RB}—————————————————————————————————————————————————————————${NC}"
-echo -e "  ${YB}- Vless${NC}                   ${GB}|${NC}  ${YB}- Websocket (CDN) non TLS${NC}"
-echo -e "  ${YB}- Vmess${NC}                   ${GB}|${NC}  ${YB}- Websocket (CDN) TLS${NC}"
-echo -e "  ${YB}- Trojan${NC}                  ${GB}|${NC}  ${YB}- gRPC (CDN) TLS${NC}"
-echo -e "  ${YB}- Socks5${NC}                  ${GB}|${NC}"
-echo -e "  ${YB}- Shadowsocks${NC}             ${GB}|${NC}"
-echo -e "  ${YB}- Shadowsocks 2022${NC}        ${GB}|${NC}"
+echo -e "\e[1;32m══════════════════Autoscript PREMIUM══════════════════\e[0m" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI SSH & OpenVPN]" | tee -a log-install.txt
+echo "    -------------------------" | tee -a log-install.txt
+echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200"  | tee -a log-install.txt
+echo "   - OpenVPN SSL             : 110"  | tee -a log-install.txt
+echo "   - Stunnel4                : 222, 777"  | tee -a log-install.txt
+echo "   - Dropbear                : 143, 109"  | tee -a log-install.txt
+echo "   - OHP Dropbear            : 8585"  | tee -a log-install.txt
+echo "   - OHP SSH                 : 8686"  | tee -a log-install.txt
+echo "   - OHP OpenVPN             : 8787"  | tee -a log-install.txt
+echo "   - Websocket SSH(HTTP)     : 80"  | tee -a log-install.txt
+echo "   - Websocket SSL(HTTPS)    : 443, 2096"  | tee -a log-install.txt
+echo "   - Websocket OpenVPN       : 2097"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI Sqd, Bdvp, Ngnx]" | tee -a log-install.txt
+echo "    ---------------------------" | tee -a log-install.txt
+echo "   - DNS Client              : 443, 53, 2222"
+echo "   - DNS Server              : 443, 88"
+echo "   - Squid Proxy             : 3128, 8000 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 81"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI XRAY]"  | tee -a log-install.txt
+echo "    ----------------" | tee -a log-install.txt
+echo "   - Vmess TLS               : 443" | tee -a log-install.txt
+echo "   - Vmess None TLS          : 80" | tee -a log-install.txt
+echo "   - Vmess GRPC              : 443" | tee -a log-install.txt
+echo "   - Vless TLS               : 443" | tee -a log-install.txt
+echo "   - Vless None TLS          : 80" | tee -a log-install.txt
+echo "   - Vless GRPC              : 443" | tee -a log-install.txt
+echo "   - Trojan GRPC             : 443" | tee -a log-install.txt
+echo "   - Trojan WS               : 443" | tee -a log-install.txt
+echo "   - Trojan None TLS         : 80" | tee -a log-install.txt
+echo "   - Trojan Go               : 443" | tee -a log-install.txt
+echo "   - Shadowsocks WS TLS      : 443" | tee -a log-install.txt
+echo "   - Shadowsocks WS none TLS : 80" | tee -a log-install.txt
+echo "   - Shadowsocks gRPC        : 443" | tee -a log-install.txt
+echo "   - Wireguard               : 7070"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI CLASH FOR ANDROID (YAML)]"  | tee -a log-install.txt
+echo "   --------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Kuala_Lumpur (GMT +8)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +8" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo -e "\e[1;32m══════════════════Autoscript By JengkolOnline══════════════════\e[0m" | tee -a log-install.txt
 echo -e "${RB}————————————————————————————————————————————————————————${NC}"
 echo -e "               ${GB}»»» Network Port Service «««${NC}             "
 echo -e "${RB}————————————————————————————————————————————————————————${NC}"
